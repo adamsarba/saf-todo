6e637152d9374d02b1d63f3b7a3ae31a30bf3407
Adam Sarba <adam.sarba@rolv.pl>
2022-04-19 14:18:58 +0200

    major lifecycle fixes

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # SAF To Do List
-![version](https://img.shields.io/badge/version-0%2e2_alpha-green.svg)
+![version](https://img.shields.io/badge/version-0%2e3_alpha-green.svg)
 
 Just run it in your browser. Simple As F*uck!
 
--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 window.addEventListener('load', function() {
 
   // Get DOM elements
+
   const form      = document.querySelector('form')
   const input     = document.querySelector('[name="new_todo"]')
   const todoList  = document.getElementById('todos')
 
   const todoData  = []
-  const doneData  = []
-  const samples   = ["Apple", "Orange", "Grapes", "Melon", "Watermelon", "Tangerine", "Lemon", "Banana", "Pineapple", "Mango"]
+  const samples   = [
+    { text: "Apple",      isDone: false },
+    { text: "Orange",     isDone: false },
+    { text: "Grapes",     isDone: false },
+    { text: "Pineapple",  isDone: false },
+    { text: "Melon",      isDone: false },
+    { text: "Watermelon", isDone: false },
+    { text: "Tangerine",  isDone: false },
+    { text: "Lemon",      isDone: true },
+    { text: "Banana",     isDone: true },
+    { text: "Mango",      isDone: true }
+  ]
 
   const btns      = document.querySelector('.btns')
   const clearLast = document.getElementById('clearLast')
@@ -17,22 +28,18 @@ window.addEventListener('load', function() {
 
 
   // Side Effects / Lifecycle
+
   let index = 0
 
   const existingTodos = JSON.parse(localStorage.getItem('todos')) || []
-  const existingDones = JSON.parse(localStorage.getItem('dones')) || []
 
   if (existingTodos > [] ) {
     console.log('Existing Todos:')
-    existingTodos.forEach(todo => {
-      addExisting(todo)
-      console.log('✔︎ ' + todo)
-    });
-  }
 
-  if (existingDones > [] ) {
-    existingDones.forEach(done => {
-      addDone(done)
+    existingTodos.forEach(item => {
+      addExisting(item)
+
+      console.log('✔︎ ' + item.text)
     });
   }
 
@@ -40,47 +47,57 @@ window.addEventListener('load', function() {
 
   // Functions
 
-  function sampleData() {
+  function addSampleData() {
     samples.forEach(sample => {
+      const todoText = sample.text
+      const isDone = sample.isDone
+
       const li  = document.createElement('li')
       todoList.appendChild(li)
-      li.innerHTML = sample
+      li.innerHTML = todoText
       li.classList.add('item')
       addBtns(li)
+      if (isDone == true) {
+        li.classList.add('done')
+      }
 
       todoData.push(sample)
       localStorage.setItem('todos', JSON.stringify(todoData))
-
-      doneData.push('unchecked')
-      localStorage.setItem('dones', JSON.stringify(doneData))
     });
   }
 
-  function addExisting(todoText) {
+  function addExisting(i) {
+    const todoText = i.text
+    const isDone = i.isDone
+
     const li  = document.createElement('li')
     todoList.appendChild(li)
     li.innerHTML = todoText
     li.classList.add('item')
     addBtns(li)
+    if (isDone == true) {
+      li.classList.add('done')
+    }
 
-    todoData.push(todoText)
+    todoData.push(i)
     localStorage.setItem('todos', JSON.stringify(todoData))
   }
 
-  function addDone(i) {
-    if ( !isNaN(i) ) {
-      const li = document.querySelector('#todos li:nth-child(' + i + ')');
-      li.classList.add('done')
-
-      doneData.push(i)
-      localStorage.setItem('dones', JSON.stringify(doneData))
-    } else {
-      doneData.push('unchecked')
-      localStorage.setItem('dones', JSON.stringify(doneData))
-    }
-  }
+  // function addDone(i) {
+  //   if ( !isNaN(i) ) {
+  //     const li = document.querySelector('#todos li:nth-child(' + i + ')');
+  //     li.classList.add('done')
+  //
+  //     doneData.push(i)
+  //     localStorage.setItem('dones', JSON.stringify(doneData))
+  //   } else {
+  //     doneData.push('unchecked')
+  //     localStorage.setItem('dones', JSON.stringify(doneData))
+  //   }
+  // }
 
   function addTodo(todoText) {
+    var item = { "text": todoText, "isDone": false}
     const li  = document.createElement('li')
     todoList.appendChild(li)
     li.innerHTML = todoText
@@ -88,11 +105,8 @@ window.addEventListener('load', function() {
     li.classList.add('new')
     addBtns(li)
 
-    todoData.push(todoText)
+    todoData.push(item)
     localStorage.setItem('todos', JSON.stringify(todoData))
-
-    doneData.push('unchecked')
-    localStorage.setItem('dones', JSON.stringify(doneData))
 
     addBtn = document.querySelector('[type="submit"]')
     addBtn.classList.add('added')
@@ -107,22 +121,24 @@ window.addEventListener('load', function() {
   }
 
   function doneTodo() {
-    parent = this.parentElement
-    var index = Array.from(parent.parentNode.children).indexOf(parent)
+    item = this.parentElement
+    var index = Array.from(item.parentNode.children).indexOf(item)
     var id = index + 1
 
-    if (!parent.classList.contains('done')) {
-      parent.classList.add('done')
+    if (!item.classList.contains('done')) {
+      item.classList.add('done')
       console.log( '✔︎ Completed item: #' + id )
 
-      doneData.splice(index, 1, id)
-      localStorage.setItem('dones', JSON.stringify(doneData))
+      todoData[index].isDone = true
+
+      localStorage.setItem('todos', JSON.stringify(todoData))
     } else {
-      parent.classList.remove('done')
+      item.classList.remove('done')
       console.log( '⨯ Unchecked item: #' + id )
 
-      doneData.splice(index, 1, 'unchecked')
-      localStorage.setItem('dones', JSON.stringify(doneData))
+      todoData[index].isDone = false
+
+      localStorage.setItem('todos', JSON.stringify(todoData))
     }
   }
 
@@ -130,8 +146,19 @@ window.addEventListener('load', function() {
     const el = this.parentElement
     const currentValue = el.innerText
 
+    console.log(el.classList)
+
+    if (el.classList.contains('done')) {
+      el.classList.remove('done')
+    }
+
     el.classList.add('active')
     el.innerHTML = '<input id="edit_todo" type="text" name="edit_todo">'
+
+    const saveBtn = document.createElement('button')
+    saveBtn.classList.add('saveBtn')
+    el.appendChild(saveBtn)
+    saveBtn.addEventListener('click', saveTodo)
 
     const deleteBtn = document.createElement('button')
     deleteBtn.classList.add('deleteBtn')
@@ -142,9 +169,25 @@ window.addEventListener('load', function() {
     editInput.value = currentValue
     editInput.focus()
 
+    editInput.addEventListener("keyup", function(event) {
+      if (event.keyCode === 13) { // Enter key
+        saveBtn.click();
+      }
+    });
+
     // TBC
     // var index = Array.from(parent.parentNode.children).indexOf(parent)
     // var id = index + 1
+  }
+
+  function saveTodo() {
+    const li = this.parentElement
+    const editInput = document.getElementById('edit_todo')
+
+    li.innerHTML = editInput.value
+    li.classList.add('item')
+    li.classList.remove('active')
+    addBtns(li)
   }
 
   function deleteLast() {
@@ -154,39 +197,29 @@ window.addEventListener('load', function() {
     setTimeout(() => {
       lastTodo.remove()
     }, 600)
-
     input.focus()
 
     todoData.pop()
     localStorage.setItem('todos', JSON.stringify(todoData))
-
-    doneData.pop()
-    localStorage.setItem('dones', JSON.stringify(doneData))
   }
 
   function deleteTodo() {
     var parent = this.parentElement
+    var index = Array.from(parent.parentNode.children).indexOf(parent)
+    var id = index + 1
+
+    console.log( '\n⨯ Deleted item: #' + id )
+    console.log('Previous number of items: ' + todoList.childElementCount);
 
     parent.classList.add('remove')
     setTimeout(() => {
       parent.remove()
+
+      console.log('Current number of items: ' + todoList.childElementCount + '\n\n')
     }, 300)
-
-    console.log('\nPrevious number of items: ' + todoList.childElementCount);
-
-    var index = Array.from(parent.parentNode.children).indexOf(parent)
-    var id = index + 1
-    console.log( '⨯ Deleted item: #' + id )
 
     todoData.splice(index, 1)
     localStorage.setItem('todos', JSON.stringify(todoData))
-
-    for (let i = 0; i < todoList.childElementCount; i++) {
-      numberOfItems = i
-    }
-    console.log('Current number of items: ' + numberOfItems + '\n\n')
-
-    // console.log(todoList.children[i]);
   }
 
   function deleteAll() {
@@ -232,12 +265,13 @@ window.addEventListener('load', function() {
 
 
   // Events
+
   form.onsubmit = (event) => {
     event.preventDefault()
     addTodo(input.value)
   }
 
-  sampleBtn.addEventListener('click', sampleData)
+  sampleBtn.addEventListener('click', addSampleData)
 
   clearLast.addEventListener('click', deleteLast)
   clearAll.addEventListener('click', deleteAll)
